d2037abcec3f448d1808c5c6d9f972dca3774c20
nakamuramano <104703112+nakamuramano@users.noreply.github.com>
2022-05-04 12:45:16 +0000

    finish3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -25,9 +25,13 @@ class ListsController < ApplicationController
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
-
   end
 
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
 
  private
 
